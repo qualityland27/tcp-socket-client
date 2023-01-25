d7820376df22e7a48ec68c365bc429a0c67031ac
qualityland <qualityland@gm.de>
2023-01-25 20:28:11 +0100

    project setup is finished

--- tcp-socket-client/src/components/HelloWorld.vue
+++ tcp-socket-client/src/components/HelloWorld.vue
@@ -8,8 +8,10 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+          rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+          rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -23,18 +25,55 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
+  <div>
+    ip:
+    <input type="text" v-model="ip" />
+    port:
+    <input type="text" v-model="port" />
+    <button @click="connect">connect</button>
+    message:
+    <input type="text" v-model="message" />
+    <button @click="sendMessage">send</button>
+  </div>
 </template>
 
 <script>
+import net from 'net'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      ip: '127.0.0.1',
+      port: '8000',
+      message: '1'
+    }
+  },
+  mounted() {
+    const socket = new net.Socket()
+    this.socket = socket
+    socket.on('data', (data) => console.log(data.toString()))
+  },
+  methods: {
+    connect() {
+      console.log("in connect")
+      this.socket.connect(this.port, this.ip)
+      console.log(this.socket)
+
+    },
+    sendMessage() {
+      console.log("in send message")
+      this.socket.write(this.message)
+    }
   }
 }
 </script>
@@ -44,14 +83,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
